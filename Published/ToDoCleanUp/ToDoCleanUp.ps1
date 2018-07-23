60e1697cf5cb51751eecf10c013c6ea26251aa7f
Andrew <kravcenko.andrejs@gmail.com>
2018-07-23 14:39:18 +0300

    Published folder

---
+++ Published/ToDoCleanUp/ToDoCleanUp.ps1
@@ -0,0 +1,10 @@
+﻿$connectionString = "Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=ToDoDB;Integrated Security=SSPI;"
+
+$connection = New-Object System.Data.SqlClient.SqlConnection
+$connection.ConnectionString = $connectionString
+$connection.Open()
+
+$query = "DELETE from ToDoItem where Completed ='true'"
+$command = $connection.CreateCommand()
+$command.CommandText = $query
+$result = $command.ExecuteReader()
